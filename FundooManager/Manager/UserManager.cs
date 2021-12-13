158f748e34bc8812ff1e967e938f2842c7211931
Shruti <shrutisablaniya93@gmail.com>
2021-12-13 13:42:37 +0530

    [Shruti] Added: Manager class for FundooManager

---
+++ FundooManager/Manager/UserManager.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooManager.Manager
+{
+    public class UserManager : IUserManager
+    {
+        private readonly IUserRepository repository;
+        public UserManager(IUserRepository repository)
+        {
+            this.repository = repository;
+        }
+        public string Register(RegisterModel user)
+        {
+            try
+            {
+                return this.repository.Register(user);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
