a678bec7809eb097b1dca3856215c8e55cfb0885
Shruti <shrutisablaniya93@gmail.com>
2021-12-13 13:45:31 +0530

    [Shruti] Added: Register Model for Fundoo Notes app

---
+++ FundooModels/RegisterModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace FundooModels
+{
+    public class RegisterModel
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        [Required]
+        // [RegularExpression("",ErrorMessage = "Email is not valid. Please enter a valid email address.")
+        public string Email { get; set; }
+        [Required]
+        public int Password { get; set; }
+    }
+}
