cbaf149dcfa45e7097252607f1b2a12b6495e7f0
Shruti <shrutisablaniya93@gmail.com>
2021-12-13 13:51:55 +0530

    [Shruti] Added: Fundoo Notes App

---
+++ FundooNotes/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using FundooManager.Interface;
+using FundooModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FundooNotes.Controllers
+{
+    public class UserController : ControllerBase
+    {
+        private readonly IUserManager manager;
+        public UserController(IUserManager manager)
+        {
+            this.manager = manager;
+        }
+
+        [HttpPost]
+        [Route("api/register")]
+        public IActionResult Register([FromBody] RegisterModel user)
+        {
+            try
+            {
+                string message =  this.manager.Register(user);
+                if(message.Equals("Register Successfull"))
+                {
+                    return this.Ok(new { Status = true, Message = message });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = message });
+                }
+            }
+            catch (Exception ex)
+            {
+                return this.NotFound(new { Status = true, ex.Message });
+            }
+        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
+    }
+}
