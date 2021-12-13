0beeac8b72f66c86c38b1dcfaa7d3b49f9ee4cdd
Shruti <shrutisablaniya93@gmail.com>
2021-12-13 13:41:15 +0530

    [Shruti] Added: Interface IUserManager for FundooManager

---
+++ FundooManager/Interface/IUserManager.cs
@@ -0,0 +1,12 @@
+﻿using FundooModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FundooManager.Interface
+{
+    public interface IUserManager
+    {
+        string Register(RegisterModel user);
+    }
+}
