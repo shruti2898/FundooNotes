73b460fa551504d6c195a08c9ebe7d09e002b228
Shruti <shrutisablaniya93@gmail.com>
2021-12-30 20:34:51 +0530

    [Shruti] Added : StyleCops for Label class files

--- FundooNotes/Controllers/LabelController.cs
+++ FundooNotes/Controllers/LabelController.cs
@@ -1,30 +1,56 @@
-﻿using FundooManager.Interface;
-using FundooModels;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="LabelController.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Shruti Sablaniya"/>
+// ----------------------------------------------------------------------------------------------------------
 namespace FundooNotes.Controllers
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using FundooManager.Interface;
+    using FundooModels;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Mvc;
+
+    /// <summary>
+    /// Controller for Label
+    /// </summary>
+    /// <seealso cref="Microsoft.AspNetCore.Mvc.ControllerBase" />
     [Authorize]
     public class LabelController : ControllerBase
     {
+        /// <summary>
+        /// The label manager
+        /// </summary>
         private readonly ILabelManager labelManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LabelController"/> class.
+        /// </summary>
+        /// <param name="labelManager">The label manager.</param>
         public LabelController(ILabelManager labelManager)
         {
             this.labelManager = labelManager;
         }
 
+        /// <summary>
+        /// Adds the labels on note.
+        /// </summary>
+        /// <param name="labelData">The label data.</param>
+        /// <returns>
+        /// Ok object result if label is added successfully
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
         [HttpPost]
         [Route("api/addLabel")]
-        public async Task<IActionResult> AddLabels([FromBody] LabelModel labelData)
+        public async Task<IActionResult> AddLabelsOnNote([FromBody] LabelModel labelData)
         {
             try
             {
-                LabelModel data = await this.labelManager.AddLabels(labelData);
+                LabelModel data = await this.labelManager.AddLabelsOnNote(labelData);
                 if (data != null)
                 {
                     return this.Ok(new ResponseModel<LabelModel> { Status = true, Message = "Label added successfully", Data = data });
@@ -40,6 +66,46 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Creates the labels for user.
+        /// </summary>
+        /// <param name="labelData">The label data.</param>
+        /// <returns>
+        /// Ok object result if label is added successfully
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
+        [HttpPost]
+        [Route("api/createLabel")]
+        public async Task<IActionResult> CreateLabelsForUser([FromBody] LabelModel labelData)
+        {
+            try
+            {
+                LabelModel data = await this.labelManager.CreateLabelsForUser(labelData);
+                if (data != null)
+                {
+                    return this.Ok(new ResponseModel<LabelModel> { Status = true, Message = "Label added successfully", Data = data });
+                }
+                else
+                {
+                    return this.BadRequest(new { Status = false, Message = "Unable to add label." });
+                }
+            }
+            catch (Exception e)
+            {
+                return this.NotFound(new { Status = true, e.Message });
+            }
+        }
+
+        /// <summary>
+        /// Gets all labels.
+        /// </summary>
+        /// <param name="userId">The user identifier.</param>
+        /// <returns>
+        /// Ok object result if all labels are retrieved successfully
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
         [HttpGet]
         [Route("api/getLabels")]
         public async Task<IActionResult> GetAllLabels(int userId)
@@ -62,6 +128,15 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Gets all notes from label.
+        /// </summary>
+        /// <param name="labelId">The label identifier.</param>
+        /// <returns>
+        /// Ok object result if all notes with same label name are retrieved successfully
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
         [HttpGet]
         [Route("api/getLabelNotes")]
         public async Task<IActionResult> GetAllNotesFromLabel(int labelId)
@@ -84,6 +159,15 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Deletes the user label.
+        /// </summary>
+        /// <param name="labelData">The label data.</param>
+        /// <returns>
+        /// Ok object result if label is deleted successfully from user
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
         [HttpDelete]
         [Route("api/deleteLabel")]
         public async Task<IActionResult> DeleteUserLabel([FromBody] LabelModel labelData)
@@ -93,7 +177,7 @@ namespace FundooNotes.Controllers
                 var result = await this.labelManager.DeleteUserLabel(labelData);
                 if (result)
                 {
-                    return this.Ok(new { Status = true, Message = "Label deleted from user successfully"});
+                    return this.Ok(new { Status = true, Message = "Label deleted from user successfully" });
                 }
                 else
                 {
@@ -106,6 +190,15 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Removes the note label.
+        /// </summary>
+        /// <param name="labelId">The label identifier.</param>
+        /// <returns>
+        /// Ok object result if label is removed successfully from note
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
         [HttpDelete]
         [Route("api/removeLabel")]
         public async Task<IActionResult> RemoveNoteLabel(int labelId)
@@ -128,6 +221,15 @@ namespace FundooNotes.Controllers
             }
         }
 
+        /// <summary>
+        /// Edits the label.
+        /// </summary>
+        /// <param name="labelData">The label data.</param>
+        /// <returns>
+        /// Ok object result if label name is edited successfully
+        /// else bad request object result
+        /// </returns>
+        /// <exception cref="System.Exception">Throws exception message as not found object result</exception>
         [HttpPut]
         [Route("api/editLabel")]
         public async Task<IActionResult> EditLabel([FromBody] LabelModel labelData)
