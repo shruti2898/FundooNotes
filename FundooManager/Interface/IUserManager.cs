f08ff0a3b34a4d7628889d15e467c397d107108f
Shruti <shrutisablaniya93@gmail.com>
2021-12-30 15:28:08 +0530

    [Shruti] Added : StyleCops for User class files

--- FundooManager/Interface/IUserManager.cs
+++ FundooManager/Interface/IUserManager.cs
@@ -1,17 +1,52 @@
-﻿using FundooModels;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IUserManager.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Shruti Sablaniya"/>
+// ----------------------------------------------------------------------------------------------------------
 namespace FundooManager.Interface
 {
+    using System.Threading.Tasks;
+    using FundooModels;
+
+    /// <summary>
+    /// User Manager Interface
+    /// </summary>
     public interface IUserManager
     {
+        /// <summary>
+        /// Registers the specified user.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <returns>User data after successful registration</returns>
         Task<RegisterModel> Register(RegisterModel user);
+
+        /// <summary>
+        /// Logins the specified user.
+        /// </summary>
+        /// <param name="loginUser">The login user.</param>
+        /// <returns>User data after logging in successfully</returns>
         Task<RegisterModel> Login(UserCredentialsModel loginUser);
+
+        /// <summary>
+        /// Resets the password.
+        /// </summary>
+        /// <param name="userCredentials">The user credentials.</param>
+        /// <returns>True if password is changed successfully else false</returns>
         Task<bool> ResetPassword(UserCredentialsModel userCredentials);
+
+        /// <summary>
+        /// Sends email for reset password.
+        /// </summary>
+        /// <param name="userEmail">The user email.</param>
+        /// <returns>True if password reset link is mailed successfully else false</returns>
         Task<bool> ForgotPassword(string userEmail);
-        string GenrateJwtToken(string email);
+
+        /// <summary>
+        /// Generates the JWT token.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns>Token string</returns>
+        string GenerateJwtToken(string email);
     }
 }
--- FundooModels/ResponseModel.cs
+++ FundooModels/ResponseModel.cs
@@ -1,13 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="ResponseModel.cs" company="Bridgelabz">
+//   Copyright © 2021 Company="BridgeLabz"
+// </copyright>
+// <creator name="Shruti Sablaniya"/>
+// ----------------------------------------------------------------------------------------------------------
 namespace FundooModels
 {
+    /// <summary>
+    /// Response Model Generic Class
+    /// </summary>
+    /// <typeparam name="T">Generic type parameter for Data field</typeparam>
     public class ResponseModel<T>
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether this <see cref="ResponseModel{T}"/> is status.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if status; otherwise, <c>false</c>.
+        /// </value>
         public bool Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message.
+        /// </summary>
+        /// <value>
+        /// The message.
+        /// </value>
         public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets the data.
+        /// </summary>
+        /// <value>
+        /// The data.
+        /// </value>
         public T Data { get; set; }
     }
 }
